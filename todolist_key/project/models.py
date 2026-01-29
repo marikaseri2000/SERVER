8bb22b1c57e7804c18552586d7442b8c94135acb
Serafina Marika Di Bari <marikaserafina@gmail.com>
2026-01-29 11:31:52 +0100

    add new user

--- todolist_key/project/models.py
+++ todolist_key/project/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 import uuid
 
 class Project(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50, unique=True)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, related_name="projects")
 
     class Meta:
         db_table = "projects"
