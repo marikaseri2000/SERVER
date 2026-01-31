5e8adc9ee4170f329f960ebb12f5049a81af7546
Serafina Marika Di Bari <marikaserafina@gmail.com>
2026-01-31 18:38:48 +0100

    feat: Add URL patterns for `presenze`, `giorni`, `partecipanti`, and `users` applications.

--- pw/pwpresenze/urls.py
+++ pw/pwpresenze/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('presenze/', include("presenze.urls")),
+    path('giorni/', include("giorni_presenze.urls")),
+    path('partecipanti/', include("partecipanti.urls")),
+    path('users/', include("users.urls")),
 ]
