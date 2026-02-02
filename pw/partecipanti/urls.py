a0a63e380b4fd4341d6f2fa879430771de0ced3f
Serafina Marika Di Bari <marikaserafina@gmail.com>
2026-02-02 18:22:23 +0100

    feat(db): add database

--- pw/partecipanti/urls.py
+++ pw/partecipanti/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("get/", views.manage_participants, name="manage_participants"),
+    path("preautorizzazione/", views.manage_participants, name="manage_participants"),
     path("me/", views.get_my_attendance_summary, name="get_my_attendance_summary"),
     path("summary/<str:partecipante_id>/", views.get_participant_summary_admin, name="get_participant_summary_admin"),
 ]
